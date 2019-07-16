f289d5ec1aef154d058b575f6c67736b814a53f5
SCAUZSJ <895104160@qq.com>
2019-07-16 22:03:56 +0800

    Add and test the api : uri:"/companies",method:GET

--- src/main/java/com/tw/apistackbase/controller/CompanyController.java
+++ src/main/java/com/tw/apistackbase/controller/CompanyController.java
@@ -2,7 +2,10 @@ package com.tw.apistackbase.controller;
 
 import com.tw.apistackbase.entity.Company;
 import com.tw.apistackbase.entity.Employee;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.*;
@@ -27,6 +30,38 @@ public class CompanyController {
         this.companies.put("E",new Company("E",2, employees));
         this.companies.put("F",new Company("F",2, employees));
 
+    }
+
+    private List<Company> convertMapToList(){
+        List<Company> companieList = new ArrayList<>();
+        for (Company company: companies.values()) {
+            companieList.add(company);
+        }
+        return companieList;
+    }
+
+    @GetMapping
+    public ResponseEntity getAll(@RequestParam(required = false)  String gender,
+                                 @RequestParam(required = false) String page ,
+                                 @RequestParam(required = false) String pageSize) {
+        List<Company> result = convertMapToList();
+//        if(gender!=null){
+//            result = result.stream().filter((obj)->{
+//                return obj.getGender().equals(gender);
+//            }).collect(Collectors.toList());
+//        }
+//        if(page!=null&&pageSize!=null){
+//            List<Employee> employeeList = new ArrayList<>();
+//            int index=(Integer.parseInt(page)-1)*Integer.parseInt(pageSize);
+//            if(index>=result.size()){
+//                result = null;
+//            }
+//            for(int i=index;i<Math.min(index+Integer.parseInt(pageSize),result.size()-1);i++){
+//                employeeList.add((Employee) result.get(i));
+//            }
+//            result = employeeList;
+//        }
+        return ResponseEntity.ok().body(result);
     }
 
 }
--- src/test/java/com/tw/apistackbase/controller/CompanyControllerTest.java
+++ src/test/java/com/tw/apistackbase/controller/CompanyControllerTest.java
@@ -1,13 +1,30 @@
 package com.tw.apistackbase.controller;
 
+import org.json.JSONArray;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 public class CompanyControllerTest {
 
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void should_return_companies_when_get() throws Exception {
+        String content = this.mockMvc.perform(get("/companies")).andExpect(status().isOk()).andReturn().getResponse().getContentAsString();
+        JSONArray json = new JSONArray(content);
+        Assertions.assertEquals("A",json.getJSONObject(0).getString("companyName"));
+    }
 }
