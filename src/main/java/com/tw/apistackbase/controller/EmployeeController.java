99de48f51e4c0293ffed51c9561433e07d89f56a
SCAUZSJ <895104160@qq.com>
2019-07-16 20:00:25 +0800

    Add and test the api : uri:"/employees",method:GET

--- src/main/java/com/tw/apistackbase/controller/EmployeeController.java
+++ src/main/java/com/tw/apistackbase/controller/EmployeeController.java
@@ -1,4 +1,25 @@
 package com.tw.apistackbase.controller;
 
+import com.tw.apistackbase.entity.Employee;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Logger;
+
+@RestController
+@RequestMapping("/employees")
 public class EmployeeController {
+
+    private final Logger log = Logger.getLogger(this.getClass().getName());
+    private Map<Integer,Employee> employees = new HashMap<>();
+
+    @GetMapping
+    public ResponseEntity getAll() {
+        this.employees.put(1,new Employee(1,"A",20,"male",6000));
+        return ResponseEntity.ok().body(employees.values().toArray());
+    }
 }
--- src/test/java/com/tw/apistackbase/controller/EmployeeControllerTest.java
+++ src/test/java/com/tw/apistackbase/controller/EmployeeControllerTest.java
@@ -1,14 +1,33 @@
 package com.tw.apistackbase.controller;
 
+import org.json.JSONArray;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 public class EmployeeControllerTest {
 
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void should_return_employees_when_get() throws Exception {
+        String content = this.mockMvc.perform(get("/employees")).andDo(print()).andExpect(status().isOk()).andReturn().getResponse().getContentAsString();
+        JSONArray json = new JSONArray(content);
+        Assertions.assertEquals(20,json.getJSONObject(0).getInt("age"));
+    }
 
 }
